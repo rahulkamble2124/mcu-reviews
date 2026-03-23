2f67d4b5fad77b1edd6330e03c30183475bc5323
rahulkamble2124 <sanikakamble0302@gmail.com>
2026-03-23 23:22:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,42 @@ function displayMovies(moviesToDisplay) {
             </div>
         `;
     });
+    function displayMovies(moviesToDisplay) {
+    const feed = document.getElementById('movieFeed');
+    feed.innerHTML = ''; 
+
+    moviesToDisplay.forEach(movie => {
+        feed.innerHTML += `
+            <div class="post">
+                <div class="post-header">${movie.title} | ${movie.phase}</div>
+                
+                <div class="video-container">
+                    <iframe width="100%" height="315" 
+                        src="https://www.youtube.com/embed/${movie.youtubeId}" 
+                        frameborder="0" allowfullscreen>
+                    </iframe>
+                </div>
+
+                <div class="post-content">
+                    <p class="release-date">Released in India: ${movie.releaseDate}</p>
+                    <p><strong>Starring:</strong> ${movie.actors.join(', ')}</p>
+                    <p class="review-text">${movie.review}</p>
+                    
+                    <div class="links">
+                        <a href="${movie.ottLink}" target="_blank" class="ott-button">Watch on OTT</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+}
+
+// Phase Filter Logic
+document.getElementById('phaseFilter').addEventListener('change', (e) => {
+    const selectedPhase = e.target.value;
+    const filtered = selectedPhase === "" ? movies : movies.filter(m => m.phase === selectedPhase);
+    displayMovies(filtered);
+});
 }
 
 // Search Feature
